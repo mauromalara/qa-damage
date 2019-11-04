aa89c1b9633f0941936e33060558a1388124db02
mauromalara <malaramauro@uch.edu.ar>
2019-11-04 14:14:48 -0300

    First commit

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Register/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Register from '@/components/Register.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Register,
+  },
+};
+</script>
